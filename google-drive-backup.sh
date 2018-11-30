0b9f9650650e93147c5a319dd04c600cdc7b3275
Wilher Distu <wilherlt@gmail.com>
2018-11-30 13:59:53 -0300

    Update google-drive-backup.sh

--- google-drive-backup.sh
+++ google-drive-backup.sh
@@ -1,2 +1,13 @@
-Aaa
+apt-get install software-properties-common dirmngr
 
+add-apt-repository ppa:alessandro-strada/ppa
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AD5F235DF639B041
+
+apt-get install google-drive-ocamlfuse
+
+
+
+# change to xeninal
+# deb http://ppa.launchpad.net/alessandro-strada/ppa/ubuntu xenial main
+# deb-src http://ppa.launchpad.net/alessandro-strada/ppa/ubuntu xenial main
