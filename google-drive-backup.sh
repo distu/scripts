7b2638e0264e971b09239f0ec4e238552fef98b1
Wilher Distu <wilherlt@gmail.com>
2018-11-30 14:49:02 -0300

    Update google-drive-backup.sh

--- google-drive-backup.sh
+++ google-drive-backup.sh
@@ -1,11 +1,9 @@
+# https://github.com/astrada/google-drive-ocamlfuse/wiki/Installation
+
 apt-get install software-properties-common dirmngr
-
 add-apt-repository ppa:alessandro-strada/ppa
-
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AD5F235DF639B041
-
 apt-get install google-drive-ocamlfuse
-
 
 
 # change to xeninal
